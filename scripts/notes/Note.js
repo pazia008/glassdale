552b3d693a0ea6c00995720f2aadf9544611ecc8
pazia008 <pazia008@gmail.com>
2021-01-05 15:57:05 -0600

    updated

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -3,10 +3,18 @@
 export const NoteHTMLConverter = (noteObject) => {
     return `
         <section class="note">
-            <div class="note__title">Suspect: ${ noteObject.suspect }</div>
+            <div class="note__title">Suspect: ${ noteObject.criminalName }</div>
             <div class="note__text">${ noteObject.text }</div>
             <div class="note__author">Author: ${ noteObject.author }</div>
             <div class="note__timestamp">Timestamp: ${ new Date(noteObject.timestamp).toLocaleDateString('en-US')  }</div>
         </section>
     `
 }
+
+
+//make a select element and populate with all criminals
+//change the HTML representation of the form
+//event listener to listen fora dropdown change
+//change how the note is saved
+//change how note objects are represented
+//modules being used = Note, NoteForm, NoteList, notes.json
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,6 +1,6 @@
 import { getNotes, useNotes } from "./NoteDataProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
-
+import { useCriminals } from "../criminals/CriminalProvider.js";
 // Query the DOM for the element that your notes will be added to 
 const contentTarget = document.querySelector(".noteList")
     // Define ye olde Evente Hubbe
@@ -13,19 +13,30 @@ eventHub.addEventListener("noteStateChanged", () => {
     NoteList()
 })
 
-const render = (noteArray) => {
+const render = (noteArray, criminals) => {
         // convert the notes objects to HTML with NoteHTMLConverter
         //map takes a function
-        const allNotesConvertedToStrings = noteArray.map((note) => NoteHTMLConverter(note)).join("")
+        const allNotesConvertedToStrings = noteArray.map((note) => {
+
+            const associatedCriminal = criminals.find(
+                (criminal) => {
+                    return criminal.id === note.criminalId
+                }
+            )
+            note.criminalName = associatedCriminal.name
+
+            return NoteHTMLConverter(note, associatedCriminal)
+        }).join("")
 
         contentTarget.innerHTML = allNotesConvertedToStrings
     } // an array is always the result of a map function
 
 // Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
 export const NoteList = () => {
+    let criminals = useCriminals()
     getNotes()
         .then(() => {
             const allNotes = useNotes()
-            render(allNotes)
+            render(allNotes, criminals)
         })
 }
